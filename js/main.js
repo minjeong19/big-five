504dba0b5bd11d237314e6d2a88839f3f7da1dbb
minjeong <kmjeongk@gmail.com>
2023-02-25 14:28:50 +0900

    질문 페이지: 버튼,진행률 초안

--- js/main.js
+++ js/main.js
@@ -1,30 +1,28 @@
 const startbtn = document.querySelector(".start");
 const homebtn = document.querySelector(".home");
+const backbtn = document.querySelector(".back");
 
 // BUTTON
-// console.log(startbtn);
-// startbtn.addEventListener("mousedown", function () {
-//   startbtn.setAttribute("src", "./img/start-touch.png");
-// });
-
-// console.log(startbtn);
-// startbtn.addEventListener("touchstart", function () {
-//   startbtn.setAttribute("src", "./img/start-touch.png");
-// });
-
-// console.log(startbtn);
-// startbtn.addEventListener("mouseup", function () {
-//   startbtn.setAttribute("src", "./img/start.png");
-// });
-
-// console.log(startbtn);
-// startbtn.addEventListener("touchend", function () {
-//   startbtn.setAttribute("src", "./img/start.png");
-// });
 startbtn.addEventListener("pointerdown", function () {
   startbtn.setAttribute("src", "./img/start-touch.png");
 });
 
 startbtn.addEventListener("pointerup", function () {
   startbtn.setAttribute("src", "./img/start.png");
+});
+
+homebtn.addEventListener("pointerdown", function () {
+  homebtn.setAttribute("src", "./img/home-touch.png");
+});
+
+homebtn.addEventListener("pointerup", function () {
+  homebtn.setAttribute("src", "./img/home.png");
+});
+
+backbtn.addEventListener("pointerdown", function () {
+  backbtn.setAttribute("src", "./img/back-touch.png");
+});
+
+backbtn.addEventListener("pointerup", function () {
+  backbtn.setAttribute("src", "./img/back.png");
 });
