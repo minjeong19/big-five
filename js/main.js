f9f9f23a48ee1bc727bb45e4946fd868cf9971ef
minjeong <kmjeongk@gmail.com>
2023-03-04 13:19:05 +0900

    완성

--- js/main.js
+++ js/main.js
@@ -1,81 +1,174 @@
-const startbtn = document.querySelector(".start"); //시작버튼
-const homebtn = document.querySelector(".home"); //홈버튼
-const backbtn = document.querySelector(".back"); //뒤로가기
-const content = document.querySelector(".content"); //시작페이지
-const qna = document.querySelector(".qna"); //질문페이지
-const firstQuestion = document.querySelector("#question1"); //첫번째 질문
-const secondQuestion = document.querySelector(".qna+.hide"); //두번째 질문
-const qBox = document.querySelectorAll(".qBox");
-const h2 = document.querySelector("h2");
-const result = document.querySelector(".result");
-const count = document.querySelector(".question-count");
+// 아이폰인지 확인
+let isIphone = false;
+const user = navigator.userAgent;
+if (user.includes("iPhone")) {
+  isIphone = true;
+}
 
-// const questionDivs = document.querySelectorAll(".qna .qBox"); //
-const endPoint = 60;
+//질문리스트
 const qList = [
-  "나는 상상력이 풍부하다.",
+  //개방성
   "나는 아이디어를 떠올리는 일을 즐긴다.",
-  "나는 미술관이나 문화관람을 좋아한다.",
-  "나는 보여지는 것을 중요시 한다.",
-  "나는 감정기복이 심하다.",
-  "나는 타인의 감정에 영향을 많이 받는다.",
+  "나는 문화관람을 좋아한다.",
+  "나는 반복되는 일보다 새로운 일을 좋아한다.",
   "나는 여러 방면에 호기심이 많다.",
-  "나는 반복되는 일보다 새로운 일이 좋다.",
   "나는 새로운 방식을 잘 받아들인다.",
-  "나는 철학적인 대화가 좋다.",
-  "진보적질문1",
-  "진보적질문2",
+  "나는 철학적인 대화를 좋아한다.",
+
+  //성실성
+  "나는 일을 하기 전에 계획을 세운다.",
   "나는 일을 효율적으로 하는 사람이다.",
-  "나는 평소에 어떤일을 하든 자신감이 있다.",
-  "나는 일을 하기전 계획을 세운다.",
-  "나는 어떤일이든 꼼꼼하게 한다.",
-  "나는 약속을 잘 지킨다.",
-  "나는 맡은일은 포기하지 않는다.",
-  "나는 스스로에게 엄격하다.",
-  "나는 목표는 무조건 이뤄야 한다.",
   "나는 일을 미루지 않고 바로 시작한다.",
   "나는 쉽게 산만해지지 않는다.",
   "나는 신중하다는 소리를 많이 듣는다.",
-  "나는 어떤 일을 시작할 때 생각하고 행동하는 편이다.",
-  "나는 처음 만난사람들과 쉽게 친해진다. ",
-  "나는 사람들과 자주 연락한다.",
-  "나는 여러모임이 있다.",
-  "나는 나의 생각을 말하는 것을 좋아한다. ",
+  "나는 무모한 의사결정을 하지 않는다.",
+
+  //외향성
+  "나는 여러 모임이 있다.",
+  "나는 새로운 만남을 즐긴다.",
+  "나는 내 생각을 말하는 것을 좋아한다. ",
   "나는 당당한 편이다.",
-  "나는 빠르게 움직이는 편이다.",
-  "나는 동시에 여러 일을 처리할 수 있다. ",
   "나는 새로운 일을 좋아한다.",
-  "나는 모험을 즐기는 편이다.",
-  "나는 걱정이 없다.",
-  "나는 안좋은일은 빨리 잊어 버린다.",
-  "나는 남을 잘 믿는 편이다. ",
-  "나는 사람들이 착하다고 생각한다. ",
+  "나는 안 좋은 일은 빨리 잊어버린다.",
+
+  //친화성
+  "나는 남을 잘 믿는다.",
   "나는 규칙을 어기지 않는 편이다.",
-  "나는 거짓말을 잘 하지 않는다.",
-  "나는 남을 돕는 일을 좋아한다.",
-  "나는 배려심이 있는 편이다.",
+  "나는 다른 사람의 의견을 잘 수긍한다.",
   "나는 싸움을 피하는 편이다.",
-  "나는 다른 사람의 의견을 잘 수긍하는 편이다.",
-  "나는 스스로를 낮게 평가한다.",
-  "나는 나 자신에게 엄격하다.",
-  "나는 다른 사람의 슬픔을 잘 느낀다.",
+  "나는 다른 사람의 감정변화를 잘 느낀다.",
   "나는 다른 사람에게 관심이 많다. ",
-  "나는 시작하기도 전에 걱정이 앞선다.",
-  "나는 쉽게 스트레스를 받는 편이다.",
-  "나는 화가 많은 편이다.",
-  "나는 쉽게 신경이 날카로워지는 편이다. ",
+
+  //감정민감성
+  "나는 실수했을 때 쉽게 당황해한다.",
   "나는 종종 기분이 가라앉는다.",
-  "나는 혼자 있으면 우울해진다.",
-  "나는 실수했을 때 쉽게 당황해 한다.",
-  "나는 자주 부끄러워 한다.",
   "나는 충동적인 소비가 많은 편이다.",
-  "나는 팔랑귀라는 소리는 듣는 편이다.",
-  "나는 마감일자가 다가오면 불안하다.",
-  "나는 압박이 느껴지면 평정심을 유지할 수 없다.",
+  "나는 팔랑귀라는 소리를 듣는 편이다.",
+  "나는 마감 일자가 다가오면 불안하다.",
+  "나는 압박이 느껴지면 평정심을 유지하기 어렵다.",
 ];
 
+const startBtn = document.querySelector(".start"); //시작버튼
+const homeBtn = document.querySelector(".home"); //홈버튼
+const previousBtn = document.querySelector(".previous"); //뒤로가기
+
+const content = document.querySelector(".content"); //시작페이지
+const qna = document.querySelector(".qna"); //질문페이지
+
+const question = document.querySelectorAll(".question");
+
+const result = document.querySelector(".result");
+const count = document.querySelector(".question-count");
+const endPoint = 30;
+
+//질문
+//애초에 있어야 하기 때문에 함수로 쓸 필요가 없음
+for (let i = 0; i < qList.length; i++) {
+  question[i].innerText = qList[i];
+  // console.log(qList[i]);
+}
+
+const questionDivs = document.querySelectorAll(".test .question");
+
+for (let i = 1; i <= qList.length; i++) {
+  // radio 태그의 name 바꿔주기
+
+  const question = document.querySelector(`#question${i}`);
+  const radioWrap = question.querySelectorAll(".choice .radio-wrap");
+  for (let j = 0; j < 5; j++) {
+    radioWrap[j].querySelector("input").setAttribute("name", `question${i}`);
+  }
+  questionDivs[i - 1].innerText = qList[i - 1];
+}
+
+// BUTTON
+startBtn.addEventListener("pointerdown", function () {
+  startBtn.setAttribute("src", "./img/start-touch.png");
+});
+
+startBtn.addEventListener("pointerup", function () {
+  startBtn.setAttribute("src", "./img/start.png");
+});
+
+homeBtn.addEventListener("pointerdown", function () {
+  homeBtn.setAttribute("src", "./img/home-touch.png");
+});
+
+homeBtn.addEventListener("pointerup", function () {
+  homeBtn.setAttribute("src", "./img/home.png");
+});
+
+previousBtn.addEventListener("pointerdown", function () {
+  previousBtn.setAttribute("src", "./img/back-touch.png");
+});
+
+previousBtn.addEventListener("pointerup", function () {
+  previousBtn.setAttribute("src", "./img/back.png");
+});
+
+// 햄버거 메뉴 활성화
+const bar = document.querySelector(".bar");
+const menu = document.querySelector(".menu");
+bar.addEventListener("click", () => {
+  if (menu.classList.contains("hide")) {
+    menu.classList.remove("hide");
+  } else {
+    menu.classList.add("hide");
+  }
+});
+
+// Canvas 활용 트랜지션
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const questionStatus = document.querySelector(".question-status");
+const intro = document.querySelector(".intro");
+const firstQuestion = document.querySelector(".intro+.test"); //첫번째 질문
+const secondQuestion = document.querySelector(".intro+.test+.test"); //두번째 질문
+const h2 = document.querySelector("header h2");
+let locateArr = [];
+let x = 0;
+let y = 0;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+ctx.fillStyle = "#181818";
+
+function drawTransitionEffect() {
+  canvas.style.zIndex = 1;
+  for (let i = 0; i < 1500; i++) {
+    setTimeout(() => {
+      const num = 30;
+      x = Math.floor(Math.random() * (canvas.width / num)) * num;
+      y = Math.floor(Math.random() * (canvas.height / num)) * num;
+      locateArr.push([x, y]);
+      ctx.fillRect(x, y, num, num);
+    }, 10);
+  }
+}
+
+function clearTransitionEffect() {
+  for (let i = 0; i < 1500; i++) {
+    setTimeout(() => {
+      const num = 30;
+      ctx.clearRect(locateArr[i][0], locateArr[i][1], num, num);
+    }, 10);
+  }
+}
+
+function resetCanvas() {
+  setTimeout(() => {
+    locateArr = [];
+    canvas.style.zIndex = -1;
+    ctx.reset();
+  }, 500);
+}
+
 // 설명 넣어주기
-const aBox = document.querySelectorAll(".aBox");
+const answer = document.querySelectorAll(".answer");
 const explain = document.createElement("div");
 const e1 = document.createElement("p");
 const e2 = document.createElement("p");
@@ -91,78 +184,83 @@ explain.style.justifyContent = "space-between";
 explain.append(e1);
 explain.append(e2);
 
-aBox.forEach((box) => {
+answer.forEach((box) => {
   const clone = explain.cloneNode(true);
   box.appendChild(clone);
 });
 
-//질문 넣기
-//애초에 있어야 하기 때문에 함수로 쓸 필요가 없음
-for (let i = 0; i < qList.length; i++) {
-  qBox[i].innerText = qList[i];
-  // console.log(qList[i]);
-}
-
-for (let i = 1; i <= qList.length; i++) {
-  // radio 태그의 name 바꿔주기
-  const question = document.querySelector(`#question${i}`);
-  const choice = question.querySelectorAll(".aBox .select");
-  for (let j = 0; j < 5; j++) {
-    choice[j].querySelector("input").setAttribute("name", `qna${i}`);
-  }
-  qBox[i - 1].innerText = qList[i - 1];
-}
-
-//STATUSBAR
-const questionStatus = document.querySelector(".question-status");
-
-// BUTTON
-startbtn.addEventListener("pointerdown", function () {
-  startbtn.setAttribute("src", "./img/start-touch.png");
-});
-
-startbtn.addEventListener("pointerup", function () {
-  startbtn.setAttribute("src", "./img/start.png");
-});
-
-homebtn.addEventListener("pointerdown", function () {
-  homebtn.setAttribute("src", "./img/home-touch.png");
-});
-
-homebtn.addEventListener("pointerup", function () {
-  homebtn.setAttribute("src", "./img/home.png");
-});
-
-backbtn.addEventListener("pointerdown", function () {
-  backbtn.setAttribute("src", "./img/back-touch.png");
-});
-
-backbtn.addEventListener("pointerup", function () {
-  backbtn.setAttribute("src", "./img/back.png");
-});
-
-//START눌렀을 때
-startbtn.addEventListener("click", function () {
-  homebtn.classList.add("hide"); //집모양 숨기고
-  backbtn.classList.remove("hide"); //뒤로가기모양 나타나기
-  content.classList.add("hide"); // 시작하는 페이지 숨기고
-  qna.classList.remove("hide"); // 질문페이지 활성화
-
-  questionStatus.classList.remove("hide"); //진행바 나타나기
+// 테스트 페이지와 인트로 페이지 간 이동
+// 테스트 페이지와 인트로 페이지 간 이동
+function goTestPageInIphone() {
+  intro.classList.add("hide");
+  h2.innerText = "Question 1";
+  questionStatus.classList.remove("hide");
+  homeBtn.classList.add("hide");
+  previousBtn.classList.remove("hide");
   firstQuestion.classList.add("on");
   firstQuestion.classList.remove("hide");
   secondQuestion.classList.add("next");
   secondQuestion.classList.remove("hide");
-  h2.innerText = `Qestion 1`;
-  count.innerText = "0/60";
-});
+}
+
+function goTestPage() {
+  intro.classList.add("hide");
+  drawTransitionEffect();
+  setTimeout(() => {
+    clearTransitionEffect();
+    h2.innerText = "Question 1";
+    questionStatus.classList.remove("hide");
+    homeBtn.classList.add("hide");
+    previousBtn.classList.remove("hide");
+    firstQuestion.classList.add("on");
+    firstQuestion.classList.remove("hide");
+    secondQuestion.classList.add("next");
+    secondQuestion.classList.remove("hide");
+    resetCanvas();
+  }, 500);
+}
+
+function goIntroPageInIphone() {
+  firstQuestion.classList.add("hide");
+  h2.innerText = "Career Anchor";
+  questionStatus.classList.add("hide");
+  homeBtn.classList.remove("hide");
+  previousBtn.classList.add("hide");
+  firstQuestion.classList.remove("on");
+  intro.classList.remove("hide");
+  secondQuestion.classList.remove("next");
+  secondQuestion.classList.add("hide");
+}
+
+function goIntroPage() {
+  firstQuestion.classList.add("hide");
+  drawTransitionEffect();
+  setTimeout(() => {
+    clearTransitionEffect();
+    h2.innerText = "Career Anchor";
+    questionStatus.classList.add("hide");
+    homeBtn.classList.remove("hide");
+    previousBtn.classList.add("hide");
+    firstQuestion.classList.remove("on");
+    intro.classList.remove("hide");
+    secondQuestion.classList.remove("next");
+    secondQuestion.classList.add("hide");
+    resetCanvas();
+  }, 500);
+}
+
+if (isIphone) {
+  startBtn.addEventListener("mouseup", goTestPageInIphone);
+} else {
+  startBtn.addEventListener("mouseup", goTestPage);
+}
 
 //다음페이지
 let answerArr = new Array(qList.length).fill(0);
 let currentPage = 0;
 
 //다음페이지로 넘어가기
-function goNext(t) {
+function goNextPage(t) {
   const prev = document.querySelector(".prev");
   const on = document.querySelector(".on");
   const next = document.querySelector(".next");
@@ -192,7 +290,7 @@ function goNext(t) {
   questionStatus.querySelector(".progress-bar").value++;
   currentPage = questionStatus.querySelector(".progress-bar").value;
   document.querySelector(".current-percentage").style.width = `${
-    1.6 * currentPage
+    3.4 * currentPage
   }%`;
   questionStatus.querySelector(
     ".question-count"
@@ -202,31 +300,35 @@ function goNext(t) {
   answerArr[currentPage - 1] = Number(t.value);
 
   // 마지막 페이지일때
-  if (currentPage === 60) {
+  if (currentPage === 30) {
     getResult();
     return;
   }
 
   // 제목 변경
-  h2.innerText = `Qestion ${currentPage + 1}`;
+  h2.innerText = `Question ${currentPage + 1}`;
 }
 
 //이전페이지
-function goPrev() {
-  const prevQna = document.querySelector(`.qna:nth-child(${currentPage - 1})`);
+function goPrevPage() {
+  const prevPrev = document.querySelector(`.test:nth-child(${currentPage})`);
   const prev = document.querySelector(".prev");
   const on = document.querySelector(".on");
   const next = document.querySelector(".next");
   console.log(currentPage);
-  console.log(prevQna);
+  console.log(prevPrev);
   if (currentPage === 0) {
-    // goIntroPage();
+    if (isIphone) {
+      goIntroPageInIphone();
+    } else {
+      goIntroPage();
+    }
     return;
   }
 
-  if (prevQna !== null) {
-    prevQna.classList.add("prev");
-    prevQna.classList.remove("hide");
+  if (prevPrev !== null) {
+    prevPrev.classList.add("prev");
+    prevPrev.classList.remove("hide");
   }
   if (prev !== null) {
     prev.classList.add("on");
@@ -241,7 +343,7 @@ function goPrev() {
     next.classList.remove("next");
   }
 
-  if (currentPage === 60) {
+  if (currentPage === 30) {
     result.classList.add("hide");
     questionStatus.classList.remove("hide");
   }
@@ -250,45 +352,33 @@ function goPrev() {
   questionStatus.querySelector(".progress-bar").value--;
   currentPage = questionStatus.querySelector(".progress-bar").value;
   document.querySelector(".current-percentage").style.width = `${
-    1.6 * currentPage
+    3.4 * currentPage
   }%`;
-  h2.innerText = `Qestion ${currentPage + 1}`;
+  h2.innerText = `Question ${currentPage + 1}`;
   questionStatus.querySelector(
     ".question-count"
   ).innerText = `${currentPage}/${qList.length}`;
 }
 
-backbtn.addEventListener("click", goPrev);
+previousBtn.addEventListener("click", goPrevPage);
 
 //검사 결과 페이지
-
 function getResult() {
-  // let eachScore = new Array(60).fill(0);
   let eachScore = new Array(5);
   for (let i = 0; i < eachScore.length; i++) {
-    eachScore[i] = new Array(6).fill(0);
+    eachScore[i] = new Array(3).fill(0);
   }
-
   const summary = document.querySelector(".result .summary");
-  result.classList.remove("hide");
-  questionStatus.classList.add("hide");
+  // result.classList.remove("hide");
+  // questionStatus.classList.add("hide");
 
-  h2.innerText = `결과페이지`;
-  // for (let i = 0; i < answerArr.length; i++) {
-  //   const index = i % 5;
-  //   eachScore[index] += answerArr[i] * 4;
-  // }
+  const prev = document.querySelector(".prev");
 
-  //소수점 자르기
-  // for (let i = 0; i < eachScore.length; i++) {
-  //   eachScore[i] = Number(eachScore[i].toFixed(1));
-  // }
-
-  //삼중for문
+  //이중for문
   //각 성향의 점수를 더해줌
   let ansIdx = 0;
   for (let i = 0; i < 5; i++) {
-    for (let j = 0; j < 6; j++) {
+    for (let j = 0; j < 3; j++) {
       for (let k = 0; k < 2; k++) {
         eachScore[i][j] += answerArr[ansIdx++];
       }
@@ -297,88 +387,21 @@ function getResult() {
 
   //각 유형의 성향 점수
   let characterArr = [
-    [
-      "상상력",
-      "예술적 감수성",
-      "감정 존중",
-      "모험성",
-      "지적 호기심",
-      "가치 진보성",
-    ],
-    ["자신감", "계획성", "책임감", "성취욕", "자제력", "신중함"],
-    ["친밀감", "사교성", "리더십", "활동성", "흥미 추구", "명랑함"],
-    ["신뢰", "강직함", "이타주의", "협조성", "겸손함", "공감력"],
-    ["걱정", "분노", "우울", "자의식", "충동성", "심약함"],
+    ["감수성", "모험성", "호기심"],
+    ["계획성", "자제력", "신중함"],
+    ["사교성", "리더십", "활동성"],
+    ["신뢰성", "협조성", "공감력"],
+    ["자의식", "충동성", "심약함"],
   ];
-  console.log(characterArr);
 
   //각 유형의 점수로 나타냄
   let resultArr = new Array(5).fill(0);
 
-  //설명배열
-  let explainArr = [
-    [
-      "상상력이 좋아요",
-      "예술적좋아요",
-      "감정존중해요",
-      "모험성이좋아요",
-      "지적호기심이 좋아요",
-      "진보성이 좋아요",
-    ],
-    [
-      "상상력이 좋아요1",
-      "예술적좋아요1",
-      "감정존중해요1",
-      "모험성이좋아요1",
-      "지적호기심이 좋아요1",
-      "진보성이 좋아요1",
-    ],
-    [
-      "상상력이 좋아요2",
-      "예술적좋아요2",
-      "감정존중해요2",
-      "모험성이좋아요2",
-      "지적호기심이 좋아요2",
-      "진보성이 좋아요2",
-    ],
-    [
-      "상상력이 좋아요3",
-      "예술적좋아요3",
-      "감정존중해요3",
-      "모험성이좋아요3",
-      "지적호기심이 좋아요3",
-      "진보성이 좋아요3",
-    ],
-    [
-      "상상력이 좋아요4",
-      "예술적좋아요4",
-      "감정존중해요4",
-      "모험성이좋아요4",
-      "지적호기심이 좋아요4",
-      "진보성이 좋아요4",
-    ],
-  ];
-
-  let explainList = [];
   for (let i = 0; i < 5; i++) {
-    for (let j = 0; j < 6; j++) {
+    for (let j = 0; j < 3; j++) {
       resultArr[i] += eachScore[i][j];
-
-      if (eachScore[i][j] >= 6) {
-        explainList.push(explainArr[i][j]);
-      }
     }
   }
-  const explainText = document.querySelector(".result .explain");
-  explainText.innerText = explainList;
-
-  // for (let i = 0; i < eachScore.length; i++) {
-  //   for (let j = 0; j < eachScore[i].length; j++) {
-  //     console.log("eachScore [" + i + "][" + j + "]' : " + eachScore[i][j]);
-  //     console.log("eachScore [" + i + "][" + j + "]' : " + eachScore[i][j]);
-
-  //   }
-  // }
 
   // 각 유형의 합계 그래프 만들기
   for (let i = 0; i < resultArr.length; i++) {
@@ -386,55 +409,75 @@ function getResult() {
       `.graph .type:nth-child(${i + 1}) .type-graph`
     );
     setTimeout(() => {
-      typeGraph.style.width = `${resultArr[i] * (78 / 100)}%`;
+      typeGraph.style.width = `${resultArr[i] * (100 / 30) * (78 / 100)}%`;
+
       setTimeout(() => {
-        typeGraph.innerText = `${resultArr[i] * (100 / 60)}점`;
+        typeGraph.innerText = `${Number(
+          (resultArr[i] * (100 / 30)).toFixed(1)
+        )}점`;
       }, 2000);
-    }, 2000);
+    }, 1000);
 
     for (let j = 0; j < eachScore[0].length; j++) {
       const character = document.querySelector(
         `.graph .type:nth-child(${i + 1}) .type-character`
       );
-      // let text = null;
-      // if (eachScore[i][j] >= 6) {
-      //   text = document.createTextNode(
-      //     `${characterArr[i][j]} : ${eachScore[i][j] * 10} - ${
-      //       explainArr[i][j]
-      //     }`
-      //   );
-      // } else {
-      //   text = document.createTextNode(
-      //     `${characterArr[i][j]} : ${eachScore[i][j] * 10}`
-      //   );
-      // }
-      let text = document.createTextNode(
-        `${characterArr[i][j]} : ${eachScore[i][j] * 10}`
-      );
+      let text = document.createElement("div");
+      text.style.textAlign = "center";
+      // text.style.backgroundColor = "pink";
+      text.innerText = `${characterArr[i][j]}\n${eachScore[i][j] * 10}점 `;
       character.appendChild(text);
-
-      // character.innerText = `${characterArr[i][j]} : ${eachScore[i][j]}`;
     }
   }
+  // SUMMARY 내용 입력
+  const userType = resultArr.indexOf(Math.max(...resultArr));
+  if (userType === 0) {
+    summary.innerText = `당신은 개방성 성향이 높아요!`;
+  } else if (userType === 1) {
+    summary.innerText = `당신은 성실성 성향이 높아요!`;
+  } else if (userType === 2) {
+    summary.innerText = `당신은 외향성 성향이 높아요!`;
+  } else if (userType === 3) {
+    summary.innerText = `당신은 친화성 성향이 높아요!`;
+  } else if (userType === 4) {
+    summary.innerText = `당신은 감정민감성 성향이 높아요!`;
+  }
+  console.log(userType, resultArr);
+  prev.classList.add("hide");
+  prev.classList.remove("prev");
+  questionStatus.classList.add("hide");
+  if (isIphone) {
+    h2.innerText = "Result";
+    result.classList.remove("hide");
+    homeBtn.classList.remove("hide");
+    previousBtn.classList.add("hide");
+  } else {
+    drawTransitionEffect();
+    setTimeout(() => {
+      clearTransitionEffect();
+      h2.innerText = "Result";
+      result.classList.remove("hide");
+      homeBtn.classList.remove("hide");
+      previousBtn.classList.add("hide");
+      resetCanvas();
+    }, 500);
+  }
+}
 
-  //SUMMARY 내용
-  //해당 사용자의 유형을 나타냄
-  // const userType = eachScore.indexOf(Math.max(...eachScore));
-  // if (userType === 0) {
-  //   summary.innerText = `당신은 개방성이 강해요`;
-  // } else if (userType === 1) {
-  //   summary.innerText = `당신은 성실성이 강해요`;
-  // } else if (userType === 2) {
-  //   summary.innerText = `당신은 외향성이 강해요`;
-  // } else if (userType === 3) {
-  //   summary.innerText = `당신은 우호성이 강해요`;
-  // } else if (userType === 4) {
-  //   summary.innerText = `당신은 신경증이 강해요`;
-  // }
+// 각 유형 설명창 열어주는 함수
+const moreInfo = document.querySelector(".more-info");
 
-  //설명입력
-  // const explain = document.querySelector(".result .explain");
-  // explain.innerText = moreInfo.querySelector(
-  //   `.explain${userType + 1}`
-  // ).innerText;
+function getInfoOfType() {
+  moreInfo.classList.remove("hide");
+  result.classList.add("hide");
+}
+
+function closeInfoOfType() {
+  moreInfo.classList.add("hide");
+  result.classList.remove("hide");
+}
+
+// 테스트 다시 시작하는 함수
+function restartTest() {
+  location.reload();
 }
---
+++ js/share.js
@@ -0,0 +1,155 @@
+// Imgur 정보
+// Client ID  // 6f7c913df440aaa
+// Client SEC  // 7a58dcbf84283963d61b6cc9c17ed9f3692ae889
+
+// SDK를 초기화 합니다. 사용할 앱의 JavaScript 키를 설정해 주세요.
+Kakao.init("e58306c7f515389029d75ba525fae068");
+
+// SDK 초기화 여부를 판단합니다.
+console.log(Kakao.isInitialized());
+
+// 이미지 정보 전달용 변수
+let img;
+
+function capture() {
+  // 카카오 버튼을 클릭하면 body 태그 전체를 이미지로 변경해 주는 함수
+  html2canvas(document.querySelector("body"), {}).then(function (canvas) {
+    // 이미지를 jpg 파일 타입으로 변경하는 코드
+    img = canvas.toDataURL("image/jpg").split(",")[1];
+    // 이미 정보를 imgur 업로드용 함수로 전달
+    uploadImageTest(img);
+  });
+}
+
+function uploadImageTest(img) {
+  // axios 라이브러리를 이용하여 imgur api 에 캡쳐한 이미지 업로드
+  axios
+    .post(
+      "https://api.imgur.com/3/image",
+      {
+        image: img,
+        type: "base64",
+      },
+      {
+        headers: {
+          Authorization: "Client-ID 6f7c913df440aaa",
+        },
+      }
+    )
+    .then((res) => {
+      // 이미지 업로드가 끝나면 이미지 업로드 주소를 응답으로 받아옴
+      console.log(res.data.data.link);
+      // 이미지 주소를 공유해야 하므로 카카오 공유 함수에 인자로 이미지 주소 전달
+      kakaoShare(res.data.data.link);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
+
+// function kakaoShare(img) {
+//   Kakao.Link.sendDefault({
+//     objectType: "feed",
+//     content: {
+//       title: "Test-World",
+//       description: "자신의 어떤 성향이 높은지 알아보세요!",
+//       // 받은 이미지 주소를 카카오 공유!
+//       imageUrl: img,
+//       link: {
+//         mobileWebUrl: "https://posco-test-world.netlify.app",
+//         webUrl: "https://posco-test-world.netlify.app",
+//       },
+//     },
+//     buttons: [
+//       {
+//         title: "내 성향을 알아보기",
+//         link: {
+//           mobileWebUrl: "https://posco-test-world.netlify.app",
+//           webUrl: "https://posco-test-world.netlify.app",
+//         },
+//       },
+//     ],
+//     // 카카오톡 미설치 시 카카오톡 설치 경로이동
+//     installTalk: true,
+//   });
+// }
+function showLoading() {
+  // 로딩 화면을 보여줌
+  const loadingScreen = document.createElement("div");
+  loadingScreen.classList.add("loading-screen");
+  loadingScreen.innerHTML = `
+    <div class="spinner">
+      <div class="dot1"></div>
+      <div class="dot2"></div>
+    </div>
+    <div class="loading-text">로딩 중입니다...</div>
+  `;
+  document.body.appendChild(loadingScreen);
+}
+
+function hideLoading() {
+  // 로딩 화면을 숨김
+  const loadingScreen = document.querySelector(".loading-screen");
+  document.body.removeChild(loadingScreen);
+}
+
+function kakaoShare(img) {
+  showLoading(); // 로딩 중이라는 화면을 보여줌
+
+  Kakao.Link.sendDefault({
+    objectType: "feed",
+    content: {
+      title: "Test-World",
+      description: "자신의 어떤 성향이 높은지 알아보세요!",
+      // 받은 이미지 주소를 카카오 공유!
+      imageUrl: img,
+      link: {
+        mobileWebUrl: "https://posco-test-world.netlify.app",
+        webUrl: "https://posco-test-world.netlify.app",
+      },
+    },
+    buttons: [
+      {
+        title: "내 성향을 알아보기",
+        link: {
+          mobileWebUrl: "https://posco-test-world.netlify.app",
+          webUrl: "https://posco-test-world.netlify.app",
+        },
+      },
+    ],
+    // 카카오톡 미설치 시 카카오톡 설치 경로이동
+    installTalk: true,
+  }).finally(() => {
+    hideLoading(); // 로딩 중이라는 화면을 숨김
+  });
+}
+// 이미지 캡쳐, 저장하기
+function captureExport() {
+  if (isIphone) {
+    alert("아이폰에서는 불가능한 기능입니다. 화면 캡쳐를 사용하세요!");
+  } else {
+    html2canvas(document.querySelector(".capture-area"), {}).then(function (
+      canvas
+    ) {
+      // 이미지를 jpg 파일 타입으로 변경하는 코드
+      const el = document.createElement("a");
+      el.href = canvas.toDataURL("image/jpeg");
+      el.download = "Big5 결과.jpg"; //다운로드 할 파일명 설정
+      el.click();
+    });
+  }
+}
+
+// URL 클립보드에 복사하기
+function copyToClipBoard() {
+  const url = "https://posco-test-world.netlify.app";
+
+  navigator.clipboard
+    .writeText(url)
+    .then(() => {
+      alert("복사되었습니다!");
+    })
+    .catch((err) => {
+      console.log("Something went wrong", err);
+    });
+}
